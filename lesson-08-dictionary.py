d4f952d663923a01bc81c51d59e6a9f43f485d26
shankarregmi <shankarregmi@gmail.com>
2020-11-16 22:58:09 +0100

    dictionaries

---
+++ lesson-08-dictionary.py
@@ -0,0 +1,25 @@
+# JavaScript Objects
+# Key value pairs
+# Key should always be string
+
+
+obj1 = {
+    'name': 'Shankar',
+    'address': 'Nepal',
+    'age': 28
+}
+
+
+list(obj1)  # print keys ['name', 'address', 'age']
+obj1.get('name') # Shankar
+obj1.keys() # keys
+obj1.value() # values
+
+for key, value in obj1.items():
+    print('{0} : {1}'.format(key, value))
+
+
+questions = ['name', 'quest', 'favorite color']
+answers = ['lancelot', 'the holy grail', 'blue']
+for q, a in zip(questions, answers):
+    print('What is your {0}?  It is {1}.'.format(q, a))
