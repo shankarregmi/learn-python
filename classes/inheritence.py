65e9b48d2163f2ed4edc7833c08a7139920d1c33
shankarregmi <shankarregmi@gmail.com>
2020-11-20 22:29:24 +0100

    check inheritence

--- classes/inheritence.py
+++ classes/inheritence.py
@@ -1,15 +1,18 @@
 class Base:
     def func1(self):
         print('Base Class Func 1')
-    
+
     def func2(self):
         print('Base Class Func 2')
         self.func1()
+
 
 class Derived(Base):
     def func1(self):
         print('Derived Class Func 1')
 
-d = Derived()
 
-d.func2()
+d = Derived()
+print(d.__class__)  # Derived
+print('isinstance(d, Derived)', isinstance(d, Derived))  # True
+print('issubclass(Base, Derived)', issubclass(Base, Derived))  # False
